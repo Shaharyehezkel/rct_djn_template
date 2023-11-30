1ca0cd6a0ecfafd899b1991918ad785bf4187a30
Shaharyehezkel <shahary315@gmail.com>
2023-11-30 17:55:28 +0200

    first commit

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from .views import MyTokenObtainPairView
+
 urlpatterns = [
     path('', views.index),
+    path('login/', views.MyTokenObtainPairView.as_view()),
+    path('categories/', views.CategoryList.as_view()),
+    path('categories/<int:pk>/', views.CategoryDetail.as_view()),
+    path('products/', views.ProductList.as_view()),
+    path('products/<int:pk>/', views.ProductDetail.as_view()),
 ]
--- base/views.py
+++ base/views.py
@@ -1,5 +1,44 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework import generics
+from .models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 def index(req):
-    return JsonResponse('hello', safe=False)
+    return Response('hello')
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+
+        # Add custom claims
+        token['username'] = user.username
+        token['email'] = user.email
+        token['number'] = user.number
+        # ...
+
+        return token
+    
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
+class CategoryList(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class ProductList(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
